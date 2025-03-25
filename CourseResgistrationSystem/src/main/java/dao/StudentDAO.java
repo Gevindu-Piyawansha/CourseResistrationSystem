919708e240854056db3a339dfb7eb57bc9b58707
'gevindu' <'gevindu.piyawansha@gmail.com'>
2025-03-25 15:34:10 +0200

    Added StudentDAO with saveStudent method

---
+++ CourseResgistrationSystem/src/main/java/dao/StudentDAO.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+import db.DBConnection;
+import entity.Student;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author Admin
+ */
+
+public class StudentDAO {
+    public boolean saveStudent(Student student) {
+        String sql = "INSERT INTO students (name, dob, program, year, contact) VALUES (?, ?, ?, ?, ?)";
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement pst = con.prepareStatement(sql)) {
+            pst.setString(1, student.getName());
+            pst.setDate(2, Date.valueOf(student.getDob()));
+            pst.setString(3, student.getProgram());
+            pst.setInt(4, student.getYear());
+            pst.setString(5, student.getContact());
+            return pst.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
